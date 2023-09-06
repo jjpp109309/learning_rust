8ff5eb49010c6567cb5b8f45d2bafb1dd1c37c27
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-06 14:54:15 -0600

    functions

--- chapters/03_programming_concepts/functions/src/main.rs
+++ chapters/03_programming_concepts/functions/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world!");
+
+    another_fn();
+    another_function(3301);
+    another_function_2(5, 'h');
+    statements_expressions();
+    functions_w_return_values();
+}
+
+fn another_fn() {
+    println!("Another function.");
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn another_function_2(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn statements_expressions() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn functions_w_return_values() {
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
