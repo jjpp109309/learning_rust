73a43acb1ac8e9099dcee93fbb06f976455a89f0
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-07 20:10:35 -0600

    string slices

--- chapters/04_ownership/slices/src/main.rs
+++ chapters/04_ownership/slices/src/main.rs
@@ -1,3 +1,51 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+
+    let word = first_word(&s);
+    let improved_word = improved_first_word(&s[..]);
+    
+    let string_literal = "bye world!";
+    let literal_word = improved_first_word(string_literal);
+
+    // s.clear();
+
+    println!("the first word is: {}", word);
+    println!("the improved first word is: {}", improved_word);
+    println!("the literal first word is: {}", literal_word);
+}
+
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+//
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if  item == b' ' {
+//             return i
+//         }
+//     }
+//
+//     s.len()
+// }
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if  item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn improved_first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if  item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
