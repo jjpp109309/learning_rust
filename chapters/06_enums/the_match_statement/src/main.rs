d99f6acbbe84a1a041d1ad7f4355698a748288f1
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-24 18:37:02 -0600

    A simple match statement for the Coin enum

--- chapters/06_enums/the_match_statement/src/main.rs
+++ chapters/06_enums/the_match_statement/src/main.rs
@@ -1,3 +1,21 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    // with if expressions we would have to evaluate conditions to bools,
+    // with match we can equate to any type!!!
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
