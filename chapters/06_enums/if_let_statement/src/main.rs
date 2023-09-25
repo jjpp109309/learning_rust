96340e6bde714c09c2f1bf16b7af2b91949b5665
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-24 19:35:51 -0600

    if-let syntactic sugar

--- chapters/06_enums/if_let_statement/src/main.rs
+++ chapters/06_enums/if_let_statement/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // if we want one arm and ignore any other arm value we could...
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    // instead we can use if-let syntactic sugar
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    } else { // it even has an else clause :)
+        println!("No maximum configuration :(")
+    }
 }
