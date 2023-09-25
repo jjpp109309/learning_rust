0b90cd2dccbc1736c4234f3f4348fb9b026887e3
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-24 18:41:22 -0600

    match with multiple line arms

--- chapters/06_enums/the_match_statement/src/main.rs
+++ chapters/06_enums/the_match_statement/src/main.rs
@@ -6,6 +6,10 @@ enum Coin {
 }
 
 fn main() {
+    let my_coin = Coin::Penny;
+    let my_coin_value = value_in_cents(my_coin);
+
+    println!("My coin value is: ${my_coin_value}");
     
 }
 
@@ -13,7 +17,11 @@ fn value_in_cents(coin: Coin) -> u8 {
     // with if expressions we would have to evaluate conditions to bools,
     // with match we can equate to any type!!!
     match coin {
-        Coin::Penny => 1,
+        // we can have multiple lines per arm using braces syntax
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
