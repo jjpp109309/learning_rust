4dd834850bcd36e63605b7d8457e34fd785404f4
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-24 18:10:15 -0600

    enums instead of structs

--- chapters/06_enums/enums/src/main.rs
+++ chapters/06_enums/enums/src/main.rs
@@ -1,24 +1,45 @@
 enum IpAddrKind {
     V4,
-    V6
+    V6,
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddrTypes {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1")
-    };
+    // storing address data using structs
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1")
+    // };
+    //
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1")
+    // };
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1")
-    };
+    // storing address data using enums (same concept)
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    // enums variants can have different types, impossible with strucs
+    let home_types = IpAddrTypes::V4(127, 0, 0, 0);
+    let loopback_types = IpAddrTypes::V6(String::from("::1"));
+
+
 }
