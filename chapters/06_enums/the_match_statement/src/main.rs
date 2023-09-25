78f1faef59a527fb85225dad20ae409d977e434d
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-24 19:08:57 -0600

    patterns that bind to values

--- chapters/06_enums/the_match_statement/src/main.rs
+++ chapters/06_enums/the_match_statement/src/main.rs
@@ -1,15 +1,27 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // and the rest of the countries 
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn main() {
     let my_coin = Coin::Penny;
     let my_coin_value = value_in_cents(my_coin);
 
+    let my_other_coin = Coin::Quarter(UsState::Alaska);
+    let my_other_coin_value = value_in_cents(my_other_coin);
+
     println!("My coin value is: ${my_coin_value}");
+    println!("My other coin value is: ${my_other_coin_value}");
+
     
 }
 
@@ -24,6 +36,11 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
+
+
 }
