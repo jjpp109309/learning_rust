91bb30c2795b0b80a74af4aa3b796c041cb1fe31
J. Jaime Paredes <jjpp109309@gmail.com>
2023-09-29 23:51:04 -0600

    defining vectors and extracting values from them

--- chapters/08_collections/vectors/src/main.rs
+++ chapters/08_collections/vectors/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // define a vector
+    let v: Vec<i32> = Vec::new();
+
+    // define a vector with macro that infers types
+    let v2 = vec![1, 2, 3]; // type annotation not necesarry
+
+    // add elements to a vector
+    let mut v3 = Vec::new();
+
+    v3.push(5);
+    v3.push(6);
+    v3.push(7);
+    v3.push(8);
+
+    // reading elements of vectors
+    let v4 = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v4[2];
+    println!("The third element is {}", third);
 }
