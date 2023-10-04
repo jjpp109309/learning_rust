6b5acad9ce270bc67a6717828b31ceef5ab59661
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-03 20:07:06 -0600

    find max value in second list

--- chapters/10_generics/duplicate_functions/src/main.rs
+++ chapters/10_generics/duplicate_functions/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
+    let number_list_2 = vec![102, 34, 6000, 89, 54, 2, 43, 8];
     
     let largest = find_largest_number(&number_list);
-    println!("The largest number is {:?}", largest);
+    println!("The largest number is first list is {:?}", largest);
+
+    let largest_2 = find_largest_number(&number_list_2);
+    println!("The largest number in second is {:?}", largest_2);
     
 }
 
