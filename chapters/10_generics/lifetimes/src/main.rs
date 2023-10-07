a02d1797af1bcbea3f1f9d658a74eb3807173e4b
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-06 18:46:46 -0600

    borrow checker

--- chapters/10_generics/lifetimes/src/main.rs
+++ chapters/10_generics/lifetimes/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // won't compile
+    // let r;
+    //
+    // {
+    //     let x = 5;
+    //     r = &x;
+    // }
+
+    let x = 5;
+    let r = &x;
+
+    println!("r: {}", r);
 }
