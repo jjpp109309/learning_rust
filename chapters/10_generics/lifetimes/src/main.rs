fe7106735ec743fcbce92fd67ad8ed3ef769b6f5
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-06 19:02:41 -0600

    function annotated with lifetimes

--- chapters/10_generics/lifetimes/src/main.rs
+++ chapters/10_generics/lifetimes/src/main.rs
@@ -11,4 +11,20 @@ fn main() {
     let r = &x;
 
     println!("r: {}", r);
+
+    // generic lifetimes in functions
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longests string is {}", result);
+}
+
+// function with lifetime annotation syntax
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
