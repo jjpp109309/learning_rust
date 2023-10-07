6a986ad0a9a15d3dd173a214310b344816a9c2e1
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-06 19:10:08 -0600

    lifetimes examples

--- chapters/10_generics/lifetimes/src/main.rs
+++ chapters/10_generics/lifetimes/src/main.rs
@@ -18,6 +18,23 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longests string is {}", result);
+
+    // passing inputs that have different lifetimes: it compiles!
+    let string3 = String::from("abcd");
+    {
+        let string4 = "xyz";
+        let result = longest(string3.as_str(), string4);
+        println!("The longests string is {}", result);
+    }
+
+    // passing inputs that have different lifetimes: no compilation :(
+    // let string5 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string6 = String::from("xyz");
+    //     result = longest(string5.as_str(), string6.as_str());
+    // }
+    // println!("The longests string is {}", result);
 }
 
 // function with lifetime annotation syntax
