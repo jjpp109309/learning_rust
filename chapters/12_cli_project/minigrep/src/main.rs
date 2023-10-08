1bdfc8c73bffcd2c561aebe892d234a3be487a29
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-08 00:02:07 -0600

    refactor basic functionality into abstractions

--- chapters/12_cli_project/minigrep/src/main.rs
+++ chapters/12_cli_project/minigrep/src/main.rs
@@ -1,18 +1,45 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    run(config);
+}
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path})
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{}", contents);
 
+    Ok(())
 }
