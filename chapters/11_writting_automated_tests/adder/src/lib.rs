975d9e172c8c52c9d5110e8e728835bd1476abd2
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-07 20:51:47 -0600

    custom messages in tests fail event

--- chapters/11_writting_automated_tests/adder/src/lib.rs
+++ chapters/11_writting_automated_tests/adder/src/lib.rs
@@ -18,6 +18,11 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    // format!("Hello {}!", name)
+    String::from("Hello ")
+}
+
 #[cfg(test)]
 mod my_awesome_tests {
     use super::*;
@@ -45,7 +50,7 @@ mod my_awesome_tests {
             height: 1,
         };
 
-        assert!(larger.can_hold(&smaller))
+        assert!(larger.can_hold(&smaller));
     }
 
     #[test]
@@ -60,11 +65,22 @@ mod my_awesome_tests {
             height: 1,
         };
 
-        assert!(!smaller.can_hold(&larger))
+        assert!(!smaller.can_hold(&larger));
     }
 
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2))
     }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was {}",
+            result
+        );
+    }
+
 }
