99c1f27080a02dfb2317e6e78a1cb1e5be7934dd
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-07 21:44:14 -0600

    Use Result in tests

--- chapters/11_writting_automated_tests/adder/src/lib.rs
+++ chapters/11_writting_automated_tests/adder/src/lib.rs
@@ -111,4 +111,13 @@ mod my_awesome_tests {
     fn greater_than_100() {
         Guess::new(200);
     }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 }
