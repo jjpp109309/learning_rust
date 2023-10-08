e3aabecacce4c66e2fa65f7a2e2c8f9c10fb7b41
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-07 22:27:57 -0600

    show function stdout

--- chapters/11_writting_automated_tests/control/src/lib.rs
+++ chapters/11_writting_automated_tests/control/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+fn print_and_returns_10(a: i32) -> i32 {
+    println!("I got the number {}", a);
+    10
 }
 
 #[cfg(test)]
-mod tests {
+mod awesome_test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn this_test_will_pass() {
+        let value = print_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = print_and_returns_10(4);
+        assert_eq!(5, value);
     }
 }
