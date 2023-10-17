657d93071b7b2acdf932dc9ae941fdd7b8366ef5
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-16 21:31:48 -0600

    iter map collect

--- chapters/13_functional_language_features/iterators/src/main.rs
+++ chapters/13_functional_language_features/iterators/src/main.rs
@@ -5,5 +5,12 @@ fn main() {
     for val in v1_iter {
         println!("Got: {}", val);
     }
+
+    // iterators that create other iterators
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    // v1.iter().map(|x| x + 1); // does not do anything
+    let v2: Vec<i32> = v1.iter().map(|x| x+1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
 }
 
