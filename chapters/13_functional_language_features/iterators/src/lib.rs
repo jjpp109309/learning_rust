de96a221dfdc3d523b8ef86ddd2d43490a4e8c56
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-16 21:43:12 -0600

    iterators that take ownership

--- chapters/13_functional_language_features/iterators/src/lib.rs
+++ chapters/13_functional_language_features/iterators/src/lib.rs
@@ -20,3 +20,50 @@ fn iterator_demonstration() {
     assert_eq!(v1_iter.next(), Some(&3));
     assert_eq!(v1_iter.next(), None);
 }
+
+// closures that capture environments
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn filter_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            }
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(in_my_size, vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot")
+            }
+        ])
+    }
+}
