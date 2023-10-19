bc6d661fe6d2f8ca1160d6df3f82e19cff42a298
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-18 22:36:35 -0600

    create deref crate

---
+++ chapters/15_smart_pointers/boxes/src/main.rs
@@ -0,0 +1,15 @@
+// recursive type with cons lists (linked lists)
+enum List {
+    Cons(i32, Box<List>),
+    Nil
+}
+
+use crate::List::{Cons, Nil};
+fn main() {
+
+    let b = Box::new(5); // i32 box instance
+    println!("b = {}", b);
+
+    // recursive lists
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
