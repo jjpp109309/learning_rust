3b9d92d77658608220e52eaf2de9175f9a50bf33
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-19 20:26:31 -0600

    implicit dereferences

--- chapters/15_smart_pointers/deref/src/main.rs
+++ chapters/15_smart_pointers/deref/src/main.rs
@@ -1,3 +1,49 @@
+use std::ops::Deref;
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    // using box like a reference
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    // defining our smart pointer
+    struct MyBox<T>(T);
+
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    
+    // implicit calls
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // hello(&(*m)[..]) // if no deref implementation
+
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}")
 }
