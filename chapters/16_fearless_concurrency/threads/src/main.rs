2bfae78d7cfaea46b791d5739c86bf4be107f23d
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-28 13:26:35 -0600

    tx takes ownership

--- chapters/16_fearless_concurrency/threads/src/main.rs
+++ chapters/16_fearless_concurrency/threads/src/main.rs
@@ -65,6 +65,7 @@ fn main() {
     thread::spawn(move || {
         let val = String::from("hi");
         tx.send(val).unwrap();
+        // println!("val is {}", val); // send takes ownership, this won't compile
     });
 
     let received = rx.recv().unwrap();
