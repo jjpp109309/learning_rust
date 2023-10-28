f27532d60a813b215a916fd61f99714a411c5b18
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-28 13:02:32 -0600

    join handles to control execution

--- chapters/16_fearless_concurrency/threads/src/main.rs
+++ chapters/16_fearless_concurrency/threads/src/main.rs
@@ -15,4 +15,37 @@ fn main() {
         thread::sleep(Duration::from_millis(1));
     }
 
+    println!("----------------------------------");
+
+    // waiting for al lthrads to finish usin join handles
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();  // blocks thread until handle terminates
+
+    println!("----------------------------------");
+
+    // moving handle between two threads
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();  // runs handle first
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
