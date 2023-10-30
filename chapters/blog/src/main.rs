bbbd20345dd67bc76680db8960ecf299d5269b25
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-30 12:05:59 -0600

    oop design patterns

---
+++ chapters/blog/src/main.rs
@@ -0,0 +1,14 @@
+using blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.conten());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content())
+}
