089b321841f0428ca2cab4df45ed87c909283364
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-30 20:11:20 -0600

    destructurin structs

--- chapters/18_patternn_matching/pattern_syntax/src/main.rs
+++ chapters/18_patternn_matching/pattern_syntax/src/main.rs
@@ -1,3 +1,8 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
     let x = 1;
 
@@ -41,4 +46,25 @@ fn main() {
         'k'..='z' => println!("late ASCII letter"),
         _ => println!("something else"),
     }
+
+    // destructurin structs
+    let p = Point { x: 0, y: 7};
+
+    let Point { x: a, y: b} = p;
+
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    let Point { x, y} = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point {x, y: 0} => println!("On the x axis"),
+        Point {x: 0, y} => println!("On the y axis"),
+        Point {x, y } => {
+            println!("on neighter axis: ({}, {})", x, y)
+        },
+    }
+
 }
