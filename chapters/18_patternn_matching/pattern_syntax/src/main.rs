65f944c495aa60112e9b8b757adae9248729978c
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-30 20:16:59 -0600

    destructuring eums

--- chapters/18_patternn_matching/pattern_syntax/src/main.rs
+++ chapters/18_patternn_matching/pattern_syntax/src/main.rs
@@ -3,6 +3,13 @@ struct Point {
     y: i32,
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let x = 1;
 
@@ -67,4 +74,21 @@ fn main() {
         },
     }
 
+    // destructing enums
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("the quit variant has no data :(");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and y direction {}", x, y);
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to rgb({},{},{})", r, g, b);
+        }
+    }
 }
